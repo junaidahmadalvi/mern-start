e3138dafa6f8f146422526f45b69c9a5d526de9f
Junaid Ahmad <junaidahmad6469@gmail.com>
2024-04-24 12:29:30 +0500

    jwt-auth implemented

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,45 @@
+const User = require("../infra/model/user");
+const { token } = require("../lib/jwt");
+const jwt = require("jsonwebtoken");
+// module.exports = {
+const authenticatedMiddleware = async (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer || !bearer.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Authentication token is missing." });
+  }
+
+  const accessToken = bearer?.split("Bearer ")[1]?.trim();
+  try {
+    const payload = await token.verifyAccessToken(accessToken);
+
+    if (payload instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({
+        message: jwt.JsonWebTokenError || "Authentication error occured.",
+      });
+    }
+
+    const user = await User.findById(payload.id, "-password");
+
+    if (!user) {
+      return res.status(401).json({ message: "Authentication Failed." });
+    }
+
+    // check soft delete
+    if (user?.deleted_at) {
+      return res.status(401).json({ message: "Authentication Failed." });
+    }
+
+    // set user in req
+    req.user = user;
+    return next();
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  authenticatedMiddleware,
+};
--- server.js
+++ server.js
@@ -1,17 +1,50 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
+var bodyParser = require("body-parser");
 const baseUrl = process.env.BASE_URL;
-
+const { authenticatedMiddleware } = require("./middleware/auth.middleware");
 const connect = require("./config/db");
 connect();
 
-app.use(express.json());
+// custom morgan's token fro timespan log
+morgan.token("timespan", function (req, res) {
+  return new Date().toISOString();
+});
 
-// Routes
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false })); //to encode url to prevent hacking(passing threat query in query params)
+app.use(
+  morgan(
+    "Log => " +
+      ":remote-addr :method :url :status :res[content-length] - :response-time ms - :timespan"
+  )
+); //for genrating api logs
+
+//------ api testing for default ------------
+app.get(`${baseUrl}`, (req, res) => {
+  res.send("API is running...");
+});
+
+// ---- Routes -----
 const userRoutes = require("./resources/user/user.route");
 
-app.use(`${baseUrl}/user`, userRoutes);
+app.use(`${baseUrl}/auth`, userRoutes);
+
+// globel middlware for all authenticated endpoints
+app.use(authenticatedMiddleware);
+
+app.get(`${baseUrl}/authenticated`, (req, res) => {
+  res.send({
+    success: "true",
+    message: "Protected test API is running...",
+    user: req.user,
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
